db527e79d5bcb46ae669731b5de58e15db81503f
khaleddwm <benk.devweb@gmail.com>
2020-05-19 17:46:13 +0200

    symfony quest 03

---
+++ src/Controller/WildController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Auteur: Khaled Benharrat
+ * Date: 19/05/2020
+ */
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WildController extends AbstractController
+{
+    /**
+     * @Route("/wild", name="wild_index")
+    */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
